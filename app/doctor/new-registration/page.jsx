6ec47269efc69c3f05a54945f066dc709531c8e1
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-02-01 21:18:34 +0600

    feat: add Registration of Patient

--- app/doctor/new-registration/page.jsx
+++ app/doctor/new-registration/page.jsx
@@ -1,5 +1,40 @@
+"use client";
 import React from "react";
+import NewPatientForm from "./newPatientForm";
 
-export default function NewPatientRegister() {
-  return <div>NewPatientRegister</div>;
-}
+const NewRegistrationPage = () => {
+  const addPatient = async (patientData) => {
+    try {
+      const response = await fetch("/api/doctor/addPatient", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(patientData),
+      });
+
+      if (response.ok) {
+        const newPatient = await response.json();
+        alert("Patient added successfully!");
+      } else {
+        const errorData = await response.json();
+        console.error(
+          "[Response not ok] Error adding patient:",
+          errorData.error
+        );
+        alert("[Response not ok] Failed to add patient");
+      }
+    } catch (error) {
+      console.error("[Form Catch] Error adding patient:", error);
+      alert("[Form Catch] Failed to add patient");
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <NewPatientForm onSubmit={addPatient} />
+    </div>
+  );
+};
+
+export default NewRegistrationPage;
